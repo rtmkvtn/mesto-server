f95fcb7052b14c9653a44aa234bcb814a8c375e3
Artem Kovtun <rtmkovtn@ya.ru>
2020-02-22 17:42:22 +0800

    changed fs.readFile to fs.promises.readFile in all routed, done route for all * requests

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -4,16 +4,21 @@ const fs = require('fs');
 const config = require('../config.js');
 
 router.get('/users/:id', (req, res) => {
-  const reader = fs.createReadStream(`${config.dbPath}/users.json`, { encoding: 'utf8' });
-  reader.on('data', (data) => {
-    const users = JSON.parse(data);
-    const userExist = users.find((user) => user._id === req.params.id);
-    if (!userExist) {
-      res.status(404).send({ message: 'Нет пользователя с таким id' });
-      return;
-    }
-    res.send(userExist);
-  });
+  fs.promises.readFile(`${config.dbPath}/users.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const userExist = json.find((user) => user._id === req.params.id);
+      if (!userExist) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(userExist);
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
 });
 
 module.exports = router;
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const config = require('../config.js');
 
 router.get('/users', (req, res) => {
-  fs.readFile(`${config.dbPath}/users.json`, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
+  fs.promises.readFile(`${config.dbPath}/users.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .then((json) => res.send(json))
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
 });
 
 module.exports = router;
