81458ded4e0bbcba09a90135aec98faa8425d7eb
Artem Kovtun <rtmkovtn@ya.ru>
2020-03-07 13:59:12 +0800

    added .populate for owners and likes fields in all the routes for the cards

--- src/server/controllers/cards.js
+++ src/server/controllers/cards.js
@@ -14,6 +14,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
+    .populate('owner')
     .then((card) => res.send(card))
     .catch((err) => {
       next(err);
@@ -22,6 +23,7 @@ module.exports.createCard = (req, res, next) => {
 
 module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
+    .populate('owner')
     .then((card) => res.send(card))
     .catch((err) => {
       next(err);
@@ -30,6 +32,8 @@ module.exports.deleteCard = (req, res, next) => {
 
 module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .populate('owner')
+    .populate('likes')
     .then((card) => res.send(card))
     .catch((err) => {
       next(err);
@@ -38,6 +42,7 @@ module.exports.likeCard = (req, res, next) => {
 
 module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .populate('owner')
     .then((card) => res.send(card))
     .catch((err) => {
       next(err);
