fdf41c98e8564fee78da7eb07fcbf7dfc712b907
Artem Kovtun <rtmkovtn@ya.ru>
2020-03-20 10:29:53 +0800

    Added dotenv, helmet modules. Edited routes. All the routes except login and createUser are now goes after auth middleware.

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 const wrongAddress = require('./routes/wrongAddress');
@@ -14,6 +18,7 @@ const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 const skipLogs = (req, res) => res.statusCode < 399;
 
+app.use(helmet());
 app.use(morgan('combined', { stream: accessLogStream, skip: skipLogs }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,12 +34,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.listen(config.PORT, () => {});
 //  routes
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5e58a84d8f54e1e3d84be5ac',
-  };
-  next();
-});
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.use(auth);
 
 app.use('/', cardsRouter, usersRouter);
 app.use(errorsHandler);
