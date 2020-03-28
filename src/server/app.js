da58794ccb17f09124accd23f4197cd4c4874058
Artem Kovtun <rtmkovtn@ya.ru>
2020-03-28 15:11:30 +0800

    added validation with celebrate module to all routes

--- src/server/app.js
+++ src/server/app.js
@@ -2,26 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const { requestLogger, errorsLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
+const validationRouter = require('./middlewares/validation');
 const wrongAddress = require('./routes/wrongAddress');
 const { errorsHandler } = require('./middlewares/errors');
 const config = require('./config.js');
 
 const app = express();
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-const skipLogs = (req, res) => res.statusCode < 399;
 
 app.use(helmet());
 app.use(cookieParser());
-app.use(morgan('combined', { stream: accessLogStream, skip: skipLogs }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -37,11 +34,20 @@ mongoose.connect(config.dbAddress, {
 app.listen(config.PORT, () => {});
 //  routes
 
+app.use(requestLogger);
+
+app.use('/', validationRouter);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
 app.use(auth);
 
 app.use('/', cardsRouter, usersRouter);
+
+app.use(errorsLogger);
+
+app.use(errors());
+
 app.use(errorsHandler);
 app.use(wrongAddress);
---
+++ src/server/middlewares/validation.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+// validators vor /users route
+router.post('/singup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().min(1),
+  }),
+}));
+
+router.post('/singin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8),
+  }),
+}));
+
+router.post('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}));
+
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().min(1),
+  }),
+}));
+
+router.get('/users/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}));
+
+// validators for /cards route
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(1),
+  }),
+}));
+
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}));
+
+router.put('/cards/cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}));
+
+router.delete('/cards/cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}));
+
+module.exports = router;
